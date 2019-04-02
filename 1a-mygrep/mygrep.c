7873a9acb926342b221870faf62b740e7a52a7b0
zwarag <zwarag@devcontrol.org>
2019-04-02 12:40:37 +0200

    handle options

--- 1a-mygrep/mygrep.c
+++ 1a-mygrep/mygrep.c
@@ -4,38 +4,42 @@
 #include <stdio.h>
 
 static char* name;
-static int ignoreCase = 0;
-static int outputFlag = 0;
+static int ignoreCase = 1;
 static int inputFlag  = 0;
+static int outputFlag = 0;
+static char* inputFile;
+static char* outputFile;
 
 void usage() {
-    fprintf(stderr, "SYNOPSIS\n\tmygrep [-i] [-o outfile] keyword [file...]");
+    fprintf(stderr, "SYNOPSIS\n\tmygrep [-i] [-o outfile] keyword [file...]\n");
     exit(EXIT_FAILURE);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    int opt;
     name = argv[0];
-    while ((opt = getopt(argc, argv, "o:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "o:i")) != -1) {
         switch (opt) {
             case 'o':
                 outputFlag = 1;
+                outputFile = optarg;
                 break;
             case 'i':
+                ignoreCase = 0;
                 break;
-                ignoreCase = 1;
             default: /* '?' */
                 usage();
         }
     }
 
-    printf("name=%s, oflag=%d; iflag=%d;  optind=%d;\n", flags, tfnd, optind);
-
-    if (optind >= argc) {
-        fprintf(stderr, "Expected argument after options\n");
-        exit(EXIT_FAILURE);
+    if (optind < argc) {
+        //TODO: check that file exists
+        inputFlag = 1;
+        inputFile = argv[optind++];
     }
 
-    printf("name argument = %s\n", argv[optind]);	//TODO: read options
+    printf("name=%s; ignoreCase=%d; optind=%d;\noutputFlag=%d;\toutputFile=%s;\ninputFlag=%d;\tintputFile=%s;\n", name, ignoreCase, optind, outputFlag, outputFile, inputFlag, inputFile);
+
     //TODO: set up reading from file/s or stdin
     //TODO: grep stuff and return it to file or stdout
     exit(EXIT_SUCCESS);
